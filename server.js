7954a6c35dc52c578b4d1f993c45641c3794ab60
Erwee van Staden <erweevs@gmail.com>
2022-02-10 12:48:25 +0200

    added mongo db conenction

--- server.js
+++ server.js
@@ -3,9 +3,13 @@ const dotenv = require('dotenv');
 const morgan = require('morgan');
 
 const users = require('./routes/users');
+const connectToDb = require('./config/db');
 
 // load the env variables
 dotenv.config({path: './config/config.env'});
+
+// connect to the DB
+connectToDb();
 
 const app = express();
 
@@ -18,6 +22,16 @@ if(process.env.NODE_ENV === 'development'){
 app.use('/api/v1/users', users);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(`Server listening on port ${process.env.NODE_ENV} ${PORT}`);
+});
+
+// handle unhandles promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+
+    // close the server
+    server.close(() => {
+        process.exit(1);
+    });
 });
