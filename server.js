4fc7a0fd47c4ae09cfd9c11a4cc6f69b14152fe3
Erwee van Staden <erweevs@gmail.com>
2022-02-10 12:32:40 +0200

    added logging middleware

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+
 const users = require('./routes/users');
 
 // load the env variables
 dotenv.config({path: './config/config.env'});
 
 const app = express();
+
+// dev logging middleware
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'));
+}
 
 // mount the Users route
 app.use('/api/v1/users', users);
