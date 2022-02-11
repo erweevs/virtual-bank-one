c186593af57ef595f83e2ec2a61d36bb09454bdb
Erwee van Staden <erweevs@icloud.com>
2022-02-11 07:51:20 +0200

    added crud for users

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -3,15 +3,43 @@ const User = require('../models/User');
 // @desc Get all Users
 // @route GET /api/v1/users
 // @access Public
-exports.getUsers = (req,res, next) => {
-    res.status(200).json({success: true, message: 'Fetch all users'});
+exports.getUsers = async (req,res, next) => {
+    try{
+        const users = await User.find();
+        res.status(200).json({
+            success: true, 
+            count: users.length,
+            data: users
+        });
+    } catch(err){
+        res.status(400).json({ 
+            success: false
+        });
+    }    
 }
 
 // @desc Get single Users
 // @route GET /api/v1/users/:id
 // @access Public
-exports.getUser = (req,res, next) => {
-    res.status(200).json({success: true, message: `Fetch user with id ${req.params.id}`});
+exports.getUser = async (req,res, next) => {
+    try{
+        const user = await User.findById(req.params.id);
+
+        if(!user){
+            return res.status(400).json({ 
+                success: false
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            data: user
+        });
+    } catch(err){
+        res.status(400).json({ 
+            success: false
+        });
+    }    
 }
 
 // @desc Create new User
@@ -35,13 +63,52 @@ exports.createUser = async (req,res, next) => {
 // @desc Update a User
 // @route PUT /api/v1/users/:id
 // @access Private
-exports.updateUser = (req,res, next) => {
-    res.status(200).json({success: true, message: `Update User: ${req.params.id}`});
+exports.updateUser = async (req,res, next) => {
+    try{
+        const user = await User.findByIdAndUpdate(
+            req.params.id, // find by Id
+            req.body, // and update to, TODO: find a way to update the updatedAt
+            {
+                new: true,
+                runValidators: true
+            });
+
+        if (!user){
+            res.status(400).json({ 
+                success: false
+            });
+        }
+
+        res.status(200).json({
+            success: true, 
+            data: user});
+    }catch(err){
+        res.status(400).json({ 
+            success: false
+        });
+    }    
 }
 
 // @desc Deletes a User
 // @route DELETE /api/v1/users
 // @access Private
-exports.deleteUser = (req,res, next) => {
-    res.status(200).json({success: true, message: `Deleting User: ${req.params.id}`});
+exports.deleteUser = async (req,res, next) => {
+    try{
+        const user = await User.findByIdAndDelete(req.params.id);
+
+        if (!user){
+            res.status(400).json({ 
+                success: false
+            });
+        }
+
+        res.status(200).json({
+            success: true, 
+            data: {}
+        });
+    }catch(err){
+        res.status(400).json({ 
+            success: false
+        });
+    }    
 }
